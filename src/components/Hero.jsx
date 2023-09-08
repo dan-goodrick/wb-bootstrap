a4114df60a163c425bd757b5666db45d327b68d9
dan-goodrick <dannyjeee@yahoo.com>
2023-09-08 12:33:56 -0600

    updates hero and values

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ import { Col, Container, Row } from "react-bootstrap";
 
 export default function Hero({ title, lead }) {
   return (
-<Container fluid className='text-center py-5, bg-light'>
-<Row className='py-lg-5'>
-<Col>
-<h1>{title}</h1>
-<p>{lead}</p>
-</Col>
-</Row>
-</Container>
+    <Container fluid className="text-center py-5, bg-light">
+      <Row className="py-lg-5">
+        <Col lg="6" md="8" className="mx-auto">
+          <h1 className="fw-light">{title}</h1>
+          <p className="lead text-muted">{lead}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,19 +1,17 @@
-function Value({ title, description, action }) {
-  return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <p>{action}</p>
-    </div>
-  );
-}
+import { Button, Card } from "react-bootstrap";
 
 export default function Values({ values }) {
   return (
-    <section>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
       {values.map(({ title, description, action }, index) => (
-        <Value key={index} title={title} description={description} action={action} />
-      ))}
-    </section>
+      <Card.Body key={index}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="primary">{action}</Button>
+      </Card.Body>
+        ))}
+    </Card>
   );
 }
+
