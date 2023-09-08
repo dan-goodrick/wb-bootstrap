0a4a5021187f890afd673fcc5b9cb52d76b12b71
dan-goodrick <dannyjeee@yahoo.com>
2023-09-08 12:44:19 -0600

    finished lab

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,17 +1,30 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
-export default function Values({ values }) {
+function Value({ title, description, action }) {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card className="shadow-sm h-100">
       <Card.Img variant="top" src="holder.js/100px180" />
-      {values.map(({ title, description, action }, index) => (
-      <Card.Body key={index}>
+      <Card.Body className="p-4 d-flex flex-column">
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Button variant="primary">{action}</Button>
+        <Button variant="success" className="mt-auto">
+          {action}
+        </Button>
       </Card.Body>
-        ))}
     </Card>
   );
 }
 
+export default function Values({ values }) {
+  return (
+    <Container>
+      <Row>
+        {values.map(({ title, description, action }) => (
+          <Col key={title}>
+            <Value title={title} description={description} action={action} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
